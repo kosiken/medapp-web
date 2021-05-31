f5f6e0e90db99edc3cca9d18c44c4bfa40ee00a2
Kenechukwu <Allisonkosy@gmail.com>
2021-05-31 13:11:10 +0100

    Update Chat.js

--- src/Api/Chat.js
+++ src/Api/Chat.js
@@ -24,7 +24,15 @@ const Servers = {
 };
 
 class Chat {
-  constructor(user, audio = true, video = true) {
+  constructor(user, audio = true, video = true, OnMessage = () => {
+
+  }, onAddStream = () => { },
+    config = {
+      addLocalStream: false,
+      maxPeers: 2,
+     servers: Servers
+    }
+  ) {
     if (!user) {
       throw new TypeError("user must be specified");
     }
@@ -37,10 +45,18 @@ class Chat {
     this.localMediaStream = null;
     this.USE_AUDIO = audio;
     this.USE_VIDEO = video;
+    this.OnMessage = OnMessage;
+    this.onAddStream = onAddStream;
+    this.config = config
   }
 
   initialize(url) {
     this.sock = io(url);
+    if (!window.sId) window.sId = Date.now().toString()
+    //alert(window.sId)
+    this.sock.on("connect", JoinChat);
+    this.sock.on("addPeer", AddPeer)
+    this.sock.on("data-rec", HandleData);
   }
 
   async addPeer(config) {
@@ -52,45 +68,64 @@ class Chat {
       peer_id = id;
 
     if (peer_id in peers) return;
-    const peerConnection = new window.RTCPeerConnection(Servers);
+    const peerConnection = new window.RTCPeerConnection(this.config.Servers || Servers);
     peers[peer_id] = peerConnection;
 
     peerConnection.onicecandidate = function (event) {
-      if (event.candidate) {
-        signalingSocket.emit("data-tran-sock", {
-          type: "candid",
-          room: "lion",
-          id: peer_id,
-          peer_id: this.sock.id,
-          data: {
-            sdpMLineIndex: event.candidate.sdpMLineIndex,
-            candidate: event.candidate.candidate,
-          },
-        });
-      }
+      ChatObject.onIceCandidate(event, peer_id)
     };
 
     peerConnection.onaddstream = function (event) {
       // ;
       streams[peer_id] = event.stream;
+      ChatObject.onAddStream("remote", event.stream, peer_id)
     };
 
     peerConnection.ondatachannel = function (event) {
       ChatObject.onDataChannel(event, peer_id);
     };
 
-    peerConnection.addStream(this.localMediaStream);
+    if (this.localMediaStream) peerConnection.addStream(this.localMediaStream);
 
     this.dataChannels[peer_id] = peerConnection.createDataChannel("talk__data_channel");
-    
+    if (config.createOffer) {
+      let localDescription = await peerConnection.createOffer();
+      try {
+        await peerConnection.setLocalDescription(localDescription);
+      }
+      catch (err) {
+        alert("Failed to set local description")
+      }
+      signalingSocket.emit("data-tran-sock",
+        {
+
+          room: "lion", peer_id: sock.id,
+
+          id: peer_id,
+
+          type: "offer",
+
+          data: localDescription,
+
+        }
+      )
+    }
+    console.log(config, peerConnection, this)
   }
 
   async joinChat(room) {
-    if (!this.localMediaStream) {
-      this.localMediaStream = await navigator.mediaDevices.getUserMedia({
-        audio: this.USE_AUDIO,
-        video: this.USE_VIDEO,
-      });
+    if (!this.localMediaStream && this.config.addLocalStream) {
+      try {
+        this.localMediaStream = await navigator.mediaDevices.getUserMedia({
+          audio: this.USE_AUDIO,
+          video: this.USE_VIDEO,
+        });
+      }
+      catch (err) {
+        console.log(err)
+        return;
+      }
+      this.onAddStream("local", this.localMediaStream, null)
     }
     this.sock.emit("join", {
       room,
@@ -113,11 +148,122 @@ class Chat {
     channel.binaryType = "arraybuffer";
     this.remoteDataChannels[peer_id] = channel;
     channel.onopen = function (ev) {
-        ChatObject.onRemoteDataChannelOpen(peer_id)
+      ChatObject.onRemoteDataChannelOpen(peer_id)
     };
+    channel.onmessage(ChatObject.OnMessage)
   }
 
   onRemoteDataChannelOpen(peer_id) {
-      console.log("Data channel: " + peer_id + " is open")
+    console.log("Data channel: " + peer_id + " is open")
+  };
+
+
+  /**
+   * 
+   * @param {RTCPeerConnectionIceEvent} event 
+   * @param {string} peer_id 
+   */
+  onIceCandidate(event, peer_id) {
+    if (event.candidate) {
+      signalingSocket.emit("data-tran-sock", {
+        type: "candid",
+        room: "lion",
+        id: peer_id,
+        peer_id: this.sock.id,
+        data: {
+          sdpMLineIndex: event.candidate.sdpMLineIndex,
+          candidate: event.candidate.candidate,
+        },
+      });
+    }
+  }
+
+  async AddIceCandidate(config) {
+    const peer = this.peers[config.peer_id];
+    if (!peer) {
+      console.log("No peer found with id ", peer_id)
+    }
+
+    if (config.data) {
+      try {
+        let iceCandidate = new window.RTCIceCandidate(config.data);
+        let _ = await peer.addIceCandidate(iceCandidate);
+        console.log(_);
+
+      }
+      catch (err) {
+        console.log("Failed to set iceCandidate " + err.message);
+        console.log(err)
+      }
+
+      return;
+    }
+    console.log("No data")
+    console.log(config)
+  }
+
+  async handleAnswer(config) {
+    try {
+      const peer_id = config.peer_id;
+      const peers = this.peers;
+      const signalingSocket = this.sock;
+      const sessionDescription = config.data
+      const peer = peers[peer_id];
+      const desc = new window.RTCSessionDescription(config.data);
+      if (sessionDescription.type == "answer") let ans = await peer.setRemoteDescription(desc);
+      else if (desc.type === "offer") {
+
+
+        let ld = await peer.createAnswer();
+
+        await peer.setLocalDescription(ld);
+
+        signalingSocket.emit("data-tran-sock",
+          {
+            room: "lion", peer_id: signalingSocket.id,
+            id: peer_id
+            , type: "answer", data: ld,									//
+
+            //	session_description: localDescription,
+          });
+
+      }
+    } catch (err) {
+      console.log("Error occured " + err.message);
+      console.log(err)
+    }
+
+
+  }
+
+
+
+  async handleData(config) {
+    const { peer_id, type } = config;
+
+    switch (type) {
+      case "candid":
+        await this.AddIceCandidate(config)
+
+        break;
+      case "offer":
+        await this.handleAnswer(config)
+        break;
+
+
+      case "answer":
+        await this.handleAnswer(config)
+        break;
+
+
+
+      default:
+        alert(type)
+        break;
+
+    }
   }
 }
+
+
+export default Chat;
